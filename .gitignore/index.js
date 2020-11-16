11494be6ae730344cf18ee7ec5fe480a025cb05b
EMT-dev <74515393+EMT-dev@users.noreply.github.com>
2020-11-16 01:40:32 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,7 +8,7 @@ bot.on('ready', function() {
     console.log("Connected");
 });
 
-bot.loging"Nzc3NjY5NDQzNDg4NDQ4NTYy.X7GzGw.wkLwamal6pVMfqUAK6PE5vTeivQ");
+bot.loging"LAjemoIsdPkUfgS2dWXWErqWTPuWhUQV");
 
 
 bot.on('message!, message => {
