9af1dadf3edc1b420925322d295723bb5b97e8ba
EMT-dev <74515393+EMT-dev@users.noreply.github.com>
2020-11-16 02:04:14 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,7 +8,7 @@ bot.on('ready', function() {
     console.log("Connected");
 });
 
-bot.loging("d9a519f8b512d1850e5f9c2ee7a736e3abc3cee5d3c440d22670b883e1f71b49");
+bot.loging("Nzc3NjY5NDQzNDg4NDQ4NTYy.X7GzGw.dXc4Pz2k4-AER91e9YymSuzwJM0");
 
 
 bot.on('message!, message => {
